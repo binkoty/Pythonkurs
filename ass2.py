fbc8fb3e92b96000e1d45941e69b4693df6425f4
Binkoty <Binkoty@yahoo.co.uk>
2018-02-09 13:48:53 +0100

    asd

--- ass2.py
+++ ass2.py
@@ -4,18 +4,19 @@ class playingcards:
 
 class hand:
     def __init__ (self):
-        self._hand = []
+        self.hand = []
 
     def generate(self):
-        self._hand =
+
+        self.hand =
 
     def add_card(self, amount, deck):
         new_card = deck[random.randint(0, len(deck))]
-        self._hand.append(new_card)
+        self.hand.append(new_card)
 
     def droppingcards(self, index):
         for i in index:
-            self._hand.remove(self._hand[i])
+            self.hand.remove(self.hand[i])
 
-    def show_hand:
-        print (self._hand)
+    def show_hand(self):
+        print (self.hand)
