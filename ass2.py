87230d1a1ecefc45e04a34c9cbecbcb78627f382
Binkoty <Binkoty@yahoo.co.uk>
2018-02-09 14:35:17 +0100

    asd

--- ass2.py
+++ ass2.py
@@ -1,18 +1,67 @@
 import random
 
-class playingcards:
+class PlayingCard():
+    def __init__(self):
 
-class hand:
+    def give_value(self):
+        raise NotImplementedError("Missing give_value implementation")
+
+class NumberedCard (Playingcard):
+    def __init__(self, value, suit):
+        self.card = (value, suit)
+
+    def give_value(self):
+        print (self.card[0])
+
+class JackCard (Playingcard):
+    def __init__(self, suit):
+        self.card = (11, suit)
+
+    def give_value(self):
+        print (self.card[0])
+
+class QueenCard (Playingcard):
+    def __init__(self, suit):
+        self.card = (12, suit)
+
+    def give_value(self):
+        print (self.card[0])
+
+class KingCard (Playingcard):
+    def __init__(self, suit):
+        self.card = (13, suit)
+
+    def give_value(self):
+        print (self.card[0])
+
+class AceCard (Playingcard):
+    def __init__(self, suit):
+        self.card = (14, suit)
+
+    def give_value(self):
+        print (self.card[0])
+
+class Suit
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+        while len(deck) < 52:
+            for
+
+
+class Hand:
     def __init__ (self):
         self.hand = []
 
-    def generate(self):
-
-        self.hand =
+    def generate(self, handsize, deck):
+        for element in handsize:
+            self.hand.append(deck[random.randint(0, len(deck))])
 
     def add_card(self, amount, deck):
         new_card = deck[random.randint(0, len(deck))]
         self.hand.append(new_card)
+        deck.remove(new_card)
 
     def droppingcards(self, index):
         for i in index:
@@ -20,3 +69,12 @@ class hand:
 
     def show_hand(self):
         print (self.hand)
+
+
+
+
+viktor = hand()
+
+viktor.generate()
+
+viktor.showhand
