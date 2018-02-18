209f6e78f34f0816fcbcb3523346c42945164937
Binkoty <Binkoty@yahoo.co.uk>
2018-02-19 00:24:02 +0100

    asd

---
+++ doc/build/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. CardDeck documentation master file, created by
+   sphinx-quickstart on Sun Feb 18 23:30:52 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CardDeck's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: testforass2
+   :members:
+   :show-inheritance:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- test/test_1.py
+++ test/test_1.py
@@ -5,7 +5,7 @@ def test_card_value():
     assert testforass2.QueenCard(testforass2.Suits.Hearts).give_value() == 12
     assert testforass2.KingCard(testforass2.Suits.Hearts).give_value() == 13
     assert testforass2.AceCard(testforass2.Suits.Hearts).give_value() == 14
-    assert testforass2.NumberedCard(5, testforass2.Suits.Hearts).give_value == 5
+    assert testforass2.NumberedCard(5, testforass2.Suits.Hearts).give_value() == 5
 def test_suit():
     assert testforass2.Suits.Clubs == 1
     assert testforass2.Suits.Diamonds == 2
@@ -22,5 +22,5 @@ def test_HandValue():
     assert testforass2.HandValue.four_kind == 7
     assert testforass2.HandValue.straight_flush == 8
     assert testforass2.HandValue.royal_flush == 9
-#def test_deck():
+
 
